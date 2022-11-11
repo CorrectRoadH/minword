786a570ec41aed2647cbe2a596f0ffd1ec935133
CorrectRoadH <a778917369@gmail.com>
2022-11-11 15:09:09 +0800

    implement multi window

--- minword/Form1.Designer.cs
+++ minword/Form1.Designer.cs
@@ -28,61 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.新建ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.文件ToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1836, 40);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // 文件ToolStripMenuItem
-            // 
-            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.新建ToolStripMenuItem});
-            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
-            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(84, 38);
-            this.文件ToolStripMenuItem.Text = "文件";
-            // 
-            // 新建ToolStripMenuItem
-            // 
-            this.新建ToolStripMenuItem.Name = "新建ToolStripMenuItem";
-            this.新建ToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
-            this.新建ToolStripMenuItem.Text = "新建";
-            this.新建ToolStripMenuItem.Click += new System.EventHandler(this.新建ToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1836, 969);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 新建ToolStripMenuItem;
     }
 }
 
--- minword/Form1.cs
+++ minword/Form1.cs
@@ -13,72 +13,72 @@ namespace minword
 {
     public partial class Form1 : Form
     {
-        private int formCount = 1;
-        private ToolStripMenuItem subItem;
+        private static int FormCount = 1;
+        //定义此常量是为了统计MDI窗体数目，
+        MainMenu mnuMain = new MainMenu();
+        MenuItem FileMenu;
+        MenuItem NewMenu;
+        MenuItem ExitMenu;
+        MenuItem WindowMenu;
 
         public Form1()
         {
             InitializeComponent();
             this.IsMdiContainer = true;
+            this.Text = "MDI演示程序";
+            FileMenu = new MenuItem();
+            FileMenu.Text = "文件";
+            WindowMenu = new MenuItem();
+            WindowMenu.Text = "窗口(&W)";
+            WindowMenu.MenuItems.Add("堆叠(&C)", new EventHandler(Cascade_Click));
+            WindowMenu.MenuItems.Add("平铺(&V)", new EventHandler(TileH_Click));
+            WindowMenu.MdiList = true;
+            //这一句比较重要，有了这一句就可以实现在新建一个MDI窗体后会在此主菜单项下显示存在的MDI窗体菜单项
+            NewMenu = new MenuItem();
+            NewMenu.Text = "新建窗体(&N)";
+            NewMenu.Click += new EventHandler(New_Click);
+            ExitMenu = new MenuItem();
+            ExitMenu.Text = "退出(&X)";
+            ExitMenu.Click += new EventHandler(Exit_Click);
+            FileMenu.MenuItems.Add(NewMenu);
+            FileMenu.MenuItems.Add(new MenuItem("-"));
+            FileMenu.MenuItems.Add(ExitMenu);
+            mnuMain.MenuItems.Add(FileMenu);
+            mnuMain.MenuItems.Add(WindowMenu);
+            this.Menu = mnuMain;
 
         }
-        ToolStripMenuItem AddContextMenu(string text, ToolStripItemCollection cms, EventHandler callback)
+
+        private void Cascade_Click(object sender, EventArgs e)
         {
-            if (text == "-")
-            {
-                ToolStripSeparator tsp = new ToolStripSeparator();
-                cms.Add(tsp);
-                return null;
-            }
-            else if (!string.IsNullOrEmpty(text))
-            {
-                ToolStripMenuItem tsmi = new ToolStripMenuItem(text);
-                tsmi.Tag = text + "TAG";
-                if (callback != null) tsmi.Click += callback;
-                cms.Add(tsmi);
-                return tsmi;
-            }
-            return null;
+            this.LayoutMdi(MdiLayout.Cascade);
         }
-        private void 新建ToolStripMenuItem_Click(object sender, EventArgs e)
+
+        
+
+        private void TileH_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void New_Click(object sender, EventArgs e)
         {
             Form frmTemp = new Form();
             //新建一个窗体
             frmTemp.MdiParent = this;
             //定义此窗体的父窗体，从而此窗体成为一个MDI窗体
-            frmTemp.Text = "新建文档" + formCount.ToString();
+            frmTemp.Text = "新建文档" + FormCount.ToString();
             //设定MDI窗体的标题
+            FormCount++;
             frmTemp.Show();
-
-            AddContextMenu("新建文档" + formCount.ToString(), subItem.DropDownItems, new EventHandler(堆叠ToolStripMenuItem_Click));
-            formCount++;
-
+            //把此MDI窗体显示出来
         }
 
-        private void selectChildWindow()
+        private void Exit_Click(object sender, EventArgs e)
         {
-
+            this.Dispose();
+            Application.Exit();
         }
 
-        private void 堆叠ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.LayoutMdi(MdiLayout.Cascade);
-
-        }
-
-        private void 平铺ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.LayoutMdi(MdiLayout.TileHorizontal);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            subItem = AddContextMenu("窗口", menuStrip1.Items, null);
-
-            //添加子菜单 
-            AddContextMenu("堆叠", subItem.DropDownItems, new EventHandler(堆叠ToolStripMenuItem_Click));
-            AddContextMenu("平铺", subItem.DropDownItems, new EventHandler(平铺ToolStripMenuItem_Click));
-
-        }
     }
 }
